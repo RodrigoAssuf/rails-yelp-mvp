2345ad8a6090b8412386fc2a40d38f08f9bb59a4
Rodrigoassuf <rodrigoassuf@hotmail.com>
2018-11-21 18:06:12 -0200

    Feito.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    adress:       '7 Boundary St, London E2 7JE',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    adress:       '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian'
+  },
+  {
+    name:         'Madame Butterfly',
+    adress:       'Rua Barao da Torre, 420, RJ',
+    category:     'japanese'
+  },
+  {
+    name:         'Le Beq Fin',
+    adress:       'Av. Atlântica, 2.000, RJ',
+    category:     'french'
+  },
+  {
+    name:         'Asterix',
+    adress:       'Estrada da Barra, 3.500, RJ',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
